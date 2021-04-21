1a3a423dc0ba8f2567dcb4a558c6eb7b0f497974
Aaron Dhiman <aaron.dhiman@gmail.com>
2021-04-21 19:58:41 -0400

    cleanup

--- analyze-URLs.py
+++ analyze-URLs.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# https://www.ayima.com/us/insights/seo/analysing-url-parameters-with-python-for-seo.html
 
 from urllib.parse import urlparse
 from urllib.parse import parse_qs
@@ -20,16 +19,11 @@ for url in Lines:
     for param in parse_qs(parsedURL.query):
         #print(parsedURL.netloc)
         params.append(param)
-	#parameters[param] = parameters.get(param, 0) + 1
         urls[parsedURL.netloc+parsedURL.path]=params
-        #urls["domain"]["params"]="A"
 for dom in urls:
     url2add="https://"+dom+'?'
     for p in urls[dom]:
         url2add+=p+'=&'
     uniqueURLs.append(url2add)
     print(dom)
-    print(uniqueURLs)
-#print(urls["domain"])
-#print(uniqueURLs)
-#https://about.att.com:80/allpostpage.html?category=Consumer&months=All&page=1
+print(uniqueURLs)
